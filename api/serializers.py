0994187e69c6dba988cc72359d000fdba753cd23
Agrim <agrimdubey37@gmail.com>
2025-10-04 17:32:12 +0530

    Added RegEx functionality to  prevent non-generic passwords and usernames

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
 from notes.models import Note
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        validators=[RegexValidator(
+            regex=r'^[A-Za-z0-9\s]{5,20}$',
+            message="Only letters, numbers, and spaces allowed in username."
+        )]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        validators=[RegexValidator(
+            regex=r'^[A-Za-z0-9@#$%^&+=]{5,20}$',
+            message="Password may contain letters, numbers, and @#$%^&+="
+        )]
+    )
+
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
@@ -18,3 +33,4 @@ class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = ['id', 'title', 'content', 'image', 'create_date']
+
