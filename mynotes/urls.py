d8339f001273a28df9eeff023a216205b53a2900
Agrim <agrimdubey37@gmail.com>
2025-10-04 23:52:55 +0530

    Changed urlpattern for notes/urls.py

--- mynotes/urls.py
+++ mynotes/urls.py
@@ -27,8 +27,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('api.urls')),
     path('', include('notes.urls')), 
-    path('swagger/',schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
 
 if settings.DEBUG:
--- notes/urls.py
+++ notes/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<int:pk>/edit/', views.note_update, name='note_update'),
     path('<int:pk>/delete/', views.note_delete, name='note_delete'),
     path('signup/', views.signup, name='signup'),
+    path('swagger/',schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
