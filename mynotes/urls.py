5eef55c18fd3f25a6585cf1200148b2a011f115e
Agrim <agrimdubey37@gmail.com>
2025-10-05 00:09:29 +0530

    .

--- mynotes/urls.py
+++ mynotes/urls.py
@@ -6,7 +6,6 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
 schema_view = get_schema_view(
    openapi.Info(
       title="Notes API",
@@ -20,14 +19,14 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('notes.urls')), 
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('api.urls')),
     path('', include('notes.urls')), 
-
+    path('swagger/',schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
--- notes/urls.py
+++ notes/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('',views.notes_list, name='notes_list'),
     path('create/',views.note_create, name='note_create'),
     path('<int:pk>/edit/', views.note_update, name='note_update'),
     path('<int:pk>/delete/', views.note_delete, name='note_delete'),
     path('signup/', views.signup, name='signup'),
-    path('swagger/',schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
